2427d6b9d11e05f335157387ade5e9ff14778589
Preet Dha <manpreetdha+github@gmail.com>
2018-03-08 22:18:22 -0800

    Use state object and setState to store data of http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,16 @@ import { Text, View } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends React.Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios
       .get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
+
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List!</Text>
