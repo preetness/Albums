36972a3f39dc3a6085ad45bd26ea0dcfec46023c
Preet Dha <manpreetdha+github@gmail.com>
2018-03-08 17:38:52 -0800

    Create AlbumList component with dummy data, to ensure it's properly being imported in index file

---
+++ src/components/AlbumList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => (
+  <View>
+    <Text>Album List!</Text>
+  </View>
+);
+
+export default AlbumList;
