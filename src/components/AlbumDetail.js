490f7050d492e5ccf9391233509ed224071268cf
Preet Dha <manpreetdha+github@gmail.com>
2018-03-09 17:23:31 -0800

    Begin adding styling to album card details

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = props => (
-  <View>
-    <Text>{props.album.title}</Text>
-  </View>
+  <Card>
+    <CardSection>
+      <View />
+      <View style={styles.headerContentStyle}>
+        <Text>{props.album.title}</Text>
+        <Text>{props.album.artist}</Text>
+      </View>
+    </CardSection>
+  </Card>
 );
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+};
 
 export default AlbumDetail;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => <View style={styles.containerStyle} />;
+const Card = props => <View style={styles.containerStyle}>{props.children}</View>;
 
 const styles = {
   containerStyle: {
