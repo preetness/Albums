9fd94ec269f3112678ae3ec426388558f89a16df
Preet Dha <manpreetdha+github@gmail.com>
2018-03-10 17:45:11 -0800

    Add a non-functional button component to the cards

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => (
+  <TouchableOpacity>
+    <Text>Click me!</Text>;
+  </TouchableOpacity>
+);
+export default Button;
