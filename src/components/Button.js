3186e6db022d4c8d3acc59e998dcc76d98903ec9
Preet Dha <manpreetdha+github@gmail.com>
2018-03-10 18:01:15 -0800

    Add styling to button and button text in the album card

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => (
-  <TouchableOpacity>
-    <Text>Click me!</Text>;
+  <TouchableOpacity style={styles.buttonStyles}>
+    <Text style={styles.textStyle}>Click me!</Text>;
   </TouchableOpacity>
 );
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyles: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
 export default Button;
